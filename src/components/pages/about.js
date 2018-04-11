be0c826979b6e268d274c1bbd72bb9be6ce36d3b
Bill <lrpc949@gmail.com>
2018-04-10 20:16:46 -0700

    about.js

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -6,7 +6,7 @@ export default () => {
     return (
         <Grid>
             <Row>
-                <h3>Just an About Page</h3>
+                <h3>This site was cobbled together by me (Bill) and my cat, who unbeknowst to me has beenwaatchng me work. He keeps putting "cat stuff" in, and when I try and remove it, the site breaks. So, I have decided he is a better coder than me, and I leave the cat stuff alone.</h3>
             </Row>
         </Grid>
     )
